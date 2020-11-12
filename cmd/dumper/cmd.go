c4671f5b8e67177a48cc6f85aaff4f7e7fc00de4
knightXun <1004815462@qq.com>
2020-11-12 13:48:35 +0800

    fix compile error

--- cmd/dumper/cmd.go
+++ cmd/dumper/cmd.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"github.com/backup-operator/pkg/dumper"
 	"k8s.io/klog"
 	"os"
 
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
+)
+
+var (
+	config string
 )
 
 func main() {
@@ -28,4 +33,44 @@ func main() {
 		klog.Error("mysqldumper failed", zap.Error(err))
 		os.Exit(1)
 	}
+}
+
+func NewRestoreCommand() *cobra.Command {
+	command := &cobra.Command{
+		Use:          "restore",
+		Short:        "restore a mysql",
+		Example: 	  "Usage: mysqldumper restore -c conf/mydumper.ini.sample",
+		SilenceUsage: true,
+		Run: func(cmd *cobra.Command, args []string) {
+			arguments, err := dumper.ParseDumperConfig(config)
+			if err != nil {
+				klog.Fatalf("parse from config file failed")
+			}
+			dumper.Loader(arguments)
+		},
+	}
+
+	command.Flags().StringVarP(&config, "config", "c","", "config file")
+	return command
+}
+
+// NewBackupCommand return a full backup subcommand.
+func NewBackupCommand() *cobra.Command {
+	command := &cobra.Command{
+		Use:          "backup",
+		Short:        "backup a mysql",
+		Example:      "Usage: mysqldumper backup -c conf/mydumper.ini.sample",
+		SilenceUsage: true,
+		Run: func(cmd *cobra.Command, args []string) {
+			arguments, err := dumper.ParseDumperConfig(config)
+			if err != nil {
+				klog.Fatalf("parse from config file failed")
+			}
+
+			dumper.Dumper(arguments)
+		},
+	}
+
+	command.Flags().StringVarP(&config, "config", "c","", "config file")
+	return command
 }
