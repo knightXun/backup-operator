cff5009d445af8001c6717a755afa8d4b836433a
knightXun <1004815462@qq.com>
2020-11-14 17:15:16 +0800

    update hack

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -13,6 +13,6 @@ echo "$(dirname "${BASH_SOURCE[0]}")/../../../.."
 bash "${CODEGEN_PKG}/generate-groups.sh" all \
   github.com/backup-operator/pkg/client \
   github.com/backup-operator/pkg/apis \
-  "backup:v1alpha1 restore:v1alpha1" \
+  "mydumper:v1alpha1" \
   --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../.." \
   --go-header-file "${SCRIPT_ROOT}/hack/boilerplate.go.txt"
