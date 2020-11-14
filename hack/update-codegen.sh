206c76d1752723d7626b8c9ee37a3154944795f5
root <root@localhost.localdomain>
2020-11-14 17:30:00 +0800

    add pkgs clients

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -14,5 +14,5 @@ bash "${CODEGEN_PKG}/generate-groups.sh" all \
   github.com/backup-operator/pkg/client \
   github.com/backup-operator/pkg/apis \
   "mydumper:v1alpha1" \
-  --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../.." \
+  --output-base "$(dirname "${BASH_SOURCE[0]}")/../../.." \
   --go-header-file "${SCRIPT_ROOT}/hack/boilerplate.go.txt"
