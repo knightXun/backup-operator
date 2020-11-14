257bc4bdd695a740feaeb102c4bd899b50868555
knightXun <1004815462@qq.com>
2020-11-14 17:50:59 +0800

    add controllers

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -1,149 +1,148 @@
 package main
 
-//import (
-//	"context"
-//	"flag"
-//	"os"
-//	"os/signal"
-//	"syscall"
-//	"time"
-//
-//	"github.com/backup-operator/pkg/client/clientset/versioned"
-//	informers "github.com/backup-operator/pkg/client/informers/externalversions"
-//	"github.com/backup-operator/pkg/controller/backup"
-//	"github.com/backup-operator/pkg/scheme"
-//	corev1 "k8s.io/api/core/v1"
-//	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-//	"k8s.io/apimachinery/pkg/util/uuid"
-//	"k8s.io/apimachinery/pkg/util/wait"
-//	kubeinformers "k8s.io/client-go/informers"
-//	"k8s.io/client-go/kubernetes"
-//	typedv1 "k8s.io/client-go/kubernetes/typed/core/v1"
-//	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-//	"k8s.io/client-go/tools/leaderelection"
-//	"k8s.io/client-go/tools/leaderelection/resourcelock"
-//	"k8s.io/client-go/tools/record"
-//	"k8s.io/klog/v2"
-//	"sigs.k8s.io/controller-runtime/pkg/client"
-//	"sigs.k8s.io/controller-runtime/pkg/client/config"
-//)
-//
-//var (
-//	leaseDuration = 15 * time.Second
-//	renewDuration = 5 * time.Second
-//	retryPeriod   = 3 * time.Second
-//	waitDuration  = 5 * time.Second
-//)
-//
-//
-//func main() {
-//	cfg := config.GetConfigOrDie()
-//
-//	cli, err := versioned.NewForConfig(cfg)
-//	if err != nil {
-//		klog.Fatalf("failed to create versioned client: %v", err)
-//	}
-//
-//	kubeCli, err := kubernetes.NewForConfig(cfg)
-//	if err != nil {
-//		klog.Fatalf("failed to create kubernetes client: %v", err)
-//	}
-//
-//	genericCli, err := client.New(cfg, client.Options{Scheme: scheme.Scheme})
-//	if err != nil {
-//		klog.Fatalf("failed to create generic client: %v", err)
-//	}
-//
-//	ns := os.Getenv("NAMESPACE")
-//	if len(ns) == 0 {
-//		ns = corev1.NamespaceDefault
-//	}
-//
-//	hostname, err := os.Hostname()
-//	if err != nil {
-//		klog.Fatalf("unable to get hostname: %v", err)
-//	}
-//
-//	id := hostname + "_" + string(uuid.NewUUID())
-//	recorder := createRecorder(kubeCli, "nebula-controller-manager")
-//
-//	rl := &resourcelock.EndpointsLock{
-//		EndpointsMeta: metav1.ObjectMeta{
-//			Namespace: ns,
-//			Name:      "nebula-controller-manager",
-//		},
-//		Client: kubeCli.CoreV1(),
-//		LockConfig: resourcelock.ResourceLockConfig{
-//			Identity:      id,
-//			EventRecorder: recorder,
-//		},
-//	}
-//
-//	controllerCtx, cancel := context.WithCancel(context.Background())
-//	defer cancel()
-//
-//	// listen for interrupts or the Linux SIGTERM signal and cancel
-//	// our context, which the leader election code will observe and
-//	// step down
-//	ch := make(chan os.Signal, 2)
-//	signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
-//	go func() {
-//		<-ch
-//		klog.Info("Received termination, signaling shutdown")
-//		cancel()
-//		<-ch
-//		os.Exit(0)
-//	}()
-//
-//	informerFactory := informers.NewSharedInformerFactory(cli, time.Second*5)
-//	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeCli, time.Second*5)
-//
-//	onStarted := func(ctx context.Context) {
-//		backup := backup.NewBackupController(
-//			kubeCli,
-//			cli,
-//			genericCli,
-//			informerFactory,
-//			kubeInformerFactory,
-//			false,
-//			2*time.Minute,
-//			2*time.Minute,
-//			2*time.Minute,
-//		)
-//
-//
-//		go backup.Run(4, ctx.Done())
-//
-//		// Start method is non-blocking and runs all registered informers in a dedicated goroutine.
-//		kubeInformerFactory.Start(ctx.Done())
-//		informerFactory.Start(ctx.Done())
-//		<-ctx.Done()
-//	}
-//	onStopped := func() {
-//		klog.Infof("leader election lost")
-//		os.Exit(0)
-//	}
-//
-//	wait.Forever(func() {
-//		leaderelection.RunOrDie(controllerCtx, leaderelection.LeaderElectionConfig{
-//			Lock:            rl,
-//			ReleaseOnCancel: true,
-//			LeaseDuration:   leaseDuration,
-//			RenewDeadline:   renewDuration,
-//			RetryPeriod:     retryPeriod,
-//			Name:            "nebula-controller-manager",
-//			Callbacks: leaderelection.LeaderCallbacks{
-//				OnStartedLeading: onStarted,
-//				OnStoppedLeading: onStopped,
-//			},
-//		})
-//	}, waitDuration)
-//}
-//
-//// createRecorder creates event recorder.
-//func createRecorder(kubeClient kubernetes.Interface, userAgent string) record.EventRecorder {
-//	eventBroadcaster := record.NewBroadcaster()
-//	eventBroadcaster.StartLogging(klog.Infof)
-//	eventBroadcaster.StartRecordingToSink(&typedv1.EventSinkImpl{Interface: kubeClient.CoreV1().Events("")})
-//	return eventBroadcaster.NewRecorder(scheme.Scheme, corev1.EventSource{Component: userAgent})
-//}
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/backup-operator/pkg/client/clientset/versioned"
+	informers "github.com/backup-operator/pkg/client/informers/externalversions"
+	"github.com/backup-operator/pkg/controller/backup"
+	"github.com/backup-operator/pkg/scheme"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/uuid"
+	"k8s.io/apimachinery/pkg/util/wait"
+	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	typedv1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/client-go/tools/leaderelection"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+)
+
+var (
+	leaseDuration = 15 * time.Second
+	renewDuration = 5 * time.Second
+	retryPeriod   = 3 * time.Second
+	waitDuration  = 5 * time.Second
+)
+
+
+func main() {
+	cfg := config.GetConfigOrDie()
+
+	cli, err := versioned.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("failed to create versioned client: %v", err)
+	}
+
+	kubeCli, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("failed to create kubernetes client: %v", err)
+	}
+
+	genericCli, err := client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	if err != nil {
+		klog.Fatalf("failed to create generic client: %v", err)
+	}
+
+	ns := os.Getenv("NAMESPACE")
+	if len(ns) == 0 {
+		ns = corev1.NamespaceDefault
+	}
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		klog.Fatalf("unable to get hostname: %v", err)
+	}
+
+	id := hostname + "_" + string(uuid.NewUUID())
+	recorder := createRecorder(kubeCli, "nebula-controller-manager")
+
+	rl := &resourcelock.EndpointsLock{
+		EndpointsMeta: metav1.ObjectMeta{
+			Namespace: ns,
+			Name:      "nebula-controller-manager",
+		},
+		Client: kubeCli.CoreV1(),
+		LockConfig: resourcelock.ResourceLockConfig{
+			Identity:      id,
+			EventRecorder: recorder,
+		},
+	}
+
+	controllerCtx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// listen for interrupts or the Linux SIGTERM signal and cancel
+	// our context, which the leader election code will observe and
+	// step down
+	ch := make(chan os.Signal, 2)
+	signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-ch
+		klog.Info("Received termination, signaling shutdown")
+		cancel()
+		<-ch
+		os.Exit(0)
+	}()
+
+	informerFactory := informers.NewSharedInformerFactory(cli, time.Second*5)
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeCli, time.Second*5)
+
+	onStarted := func(ctx context.Context) {
+		backup := backup.NewBackupController(
+			kubeCli,
+			cli,
+			genericCli,
+			informerFactory,
+			kubeInformerFactory,
+			false,
+			2*time.Minute,
+			2*time.Minute,
+			2*time.Minute,
+		)
+
+
+		go backup.Run(4, ctx.Done())
+
+		// Start method is non-blocking and runs all registered informers in a dedicated goroutine.
+		kubeInformerFactory.Start(ctx.Done())
+		informerFactory.Start(ctx.Done())
+		<-ctx.Done()
+	}
+	onStopped := func() {
+		klog.Infof("leader election lost")
+		os.Exit(0)
+	}
+
+	wait.Forever(func() {
+		leaderelection.RunOrDie(controllerCtx, leaderelection.LeaderElectionConfig{
+			Lock:            rl,
+			ReleaseOnCancel: true,
+			LeaseDuration:   leaseDuration,
+			RenewDeadline:   renewDuration,
+			RetryPeriod:     retryPeriod,
+			Name:            "nebula-controller-manager",
+			Callbacks: leaderelection.LeaderCallbacks{
+				OnStartedLeading: onStarted,
+				OnStoppedLeading: onStopped,
+			},
+		})
+	}, waitDuration)
+}
+
+// createRecorder creates event recorder.
+func createRecorder(kubeClient kubernetes.Interface, userAgent string) record.EventRecorder {
+	eventBroadcaster := record.NewBroadcaster()
+	eventBroadcaster.StartLogging(klog.Infof)
+	eventBroadcaster.StartRecordingToSink(&typedv1.EventSinkImpl{Interface: kubeClient.CoreV1().Events("")})
+	return eventBroadcaster.NewRecorder(scheme.Scheme, corev1.EventSource{Component: userAgent})
+}
--- pkg/controller/restore/restore-controller.go
+++ pkg/controller/restore/restore-controller.go
@@ -1,204 +1,204 @@
 package Restore
 
-//import (
-//	"fmt"
-//	kubeinformers "k8s.io/client-go/informers"
-//	"k8s.io/client-go/kubernetes"
-//	"k8s.io/kubernetes/pkg/controller"
-//	"time"
-//
-//	corev1 "k8s.io/api/core/v1"
-//	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-//	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-//	"k8s.io/apimachinery/pkg/util/wait"
-//	"k8s.io/client-go/tools/cache"
-//	"k8s.io/client-go/tools/record"
-//	"k8s.io/client-go/util/workqueue"
-//	"k8s.io/klog"
-//
-//	mydumperv1alpha1 "github.com/backup-operator/pkg/apis/mydumper/v1alpha1"
-//	"github.com/backup-operator/pkg/client/clientset/versioned"
-//	clientset "github.com/backup-operator/pkg/client/clientset/versioned"
-//	informers "github.com/backup-operator/pkg/client/informers/externalversions"
-//	mydumperInformer "github.com/backup-operator/pkg/client/informers/externalversions/mydumper/v1alpha1"
-//	mydumperlister "github.com/backup-operator/pkg/client/listers/mydumper/v1alpha1"
-//	"k8s.io/client-go/kubernetes/scheme"
-//	eventv1 "k8s.io/client-go/kubernetes/typed/core/v1"
-//	"sigs.k8s.io/controller-runtime/pkg/client"
-//)
-//
-//type RestoreController struct {
-//	kubeclient  kubernetes.Interface
-//	mydumperClient  	clientset.Interface
-//
-//	mydumperLister        mydumperlister.mydumperLister
-//	mydumperInformer     mydumperInformer.mydumperInformer
-//	mydumperSynced       cache.InformerSynced
-//
-//	workqueue workqueue.RateLimitingInterface
-//
-//	recorder record.EventRecorder
-//	cli versioned.Interface
-//
-//	syncHandler   func(jobKey string) (bool, error)
-//
-//	expectations controller.ControllerExpectationsInterface
-//}
-//
-//func NewRestoreController(
-//	kubeclientset kubernetes.Interface,
-//	cli versioned.Interface,
-//	genericCli client.Client,
-//	informerFactory informers.SharedInformerFactory,
-//	kubeInformerFactory kubeinformers.SharedInformerFactory) *MydumperController {
-//
-//	utilruntime.Must(Restorescheme.AddToScheme(scheme.Scheme))
-//	klog.Info("Creating event broadcaster")
-//	eventBroadcaster := record.NewBroadcasterWithCorrelatorOptions(record.CorrelatorOptions{QPS: 1})
-//	eventBroadcaster.StartLogging(klog.V(2).Infof)
-//	eventBroadcaster.StartRecordingToSink(&eventv1.EventSinkImpl{
-//		Interface: eventv1.New(kubeclientset.CoreV1().RESTClient()).Events("")})
-//	recorder := eventBroadcaster.NewRecorder(mydumperv1alpha1.Scheme, corev1.EventSource{Component: "Restore-controller"})
-//
-//	nbInformer := informerFactory.Restore().V1alpha1().Restores()
-//
-//	controller := &RestoreController{
-//		kubeclient:     				kubeclientset,
-//		mydumperInformer:   			nbInformer,
-//		mydumperLister:        			nbInformer.Lister(),
-//		mydumperSynced:        			nbInformer.Informer().HasSynced,
-//		workqueue:         				workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "Restores"),
-//		recorder:          				recorder,
-//	}
-//
-//	controller.syncHandler = controller.syncHandler
-//
-//	klog.Info("Setting up event handlers")
-//
-//	nbInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-//		AddFunc: controller.createRestore,
-//		UpdateFunc: controller.updateRestore,
-//		DeleteFunc: controller.deleteRestore,
-//	})
-//
-//	return controller
-//}
-//
-//func (c *RestoreController) createRestore(obj interface{}) {
-//	restore, ok := obj.(*mydumperv1alpha1.Restore)
-//
-//	if !ok {
-//		return
-//	}
-//
-//	return
-//}
-//
-//func (c *RestoreController) deleteRestore(obj interface{}) {
-//
-//	Restore := obj.(*mydumperv1alpha1.Restore)
-//
-//}
-//
-//
-//func (c *RestoreController) enqueueRestore(obj interface{}) {
-//	var key string
-//	var err error
-//	if key, err = cache.MetaNamespaceKeyFunc(obj); err != nil {
-//		utilruntime.HandleError(err)
-//		return
-//	}
-//	c.workqueue.Add(key)
-//}
-//
-//func (c *RestoreController) handleObject(obj interface{}) {
-//	var object metav1.Object
-//	var ok bool
-//	if object, ok = obj.(metav1.Object); !ok {
-//		tombstone, ok := obj.(cache.DeletedFinalStateUnknown)
-//		if !ok {
-//			utilruntime.HandleError(fmt.Errorf("error decoding object, invalid type"))
-//			return
-//		}
-//		object, ok = tombstone.Obj.(metav1.Object)
-//		if !ok {
-//			utilruntime.HandleError(fmt.Errorf("error decoding object tombstone, invalid type"))
-//			return
-//		}
-//		klog.Info("Recovered deleted object '%s' from tombstone", object.GetName())
-//	}
-//	klog.Info("Processing object: %s", object.GetName())
-//	if ownerRef := metav1.GetControllerOf(object); ownerRef != nil {
-//		// If this object is not owned by a Foo, we should not do anything more
-//		// with it.
-//		if ownerRef.Kind != "Restore" {
-//			return
-//		}
-//
-//		Restore, err := c.mydumperLister.Restore(object.GetNamespace()).Get(ownerRef.Name)
-//		if err != nil {
-//			klog.Info("ignoring orphaned object '%s' of Restore '%s'", object.GetSelfLink(), ownerRef.Name)
-//			return
-//		}
-//
-//		c.enqueueRestore(Restore)
-//		return
-//	}
-//}
-//
-//func (c *RestoreController) Run(workers int, stopCh <-chan struct{}) {
-//	defer utilruntime.HandleCrash()
-//	defer c.workqueue.ShutDown()
-//
-//	klog.Info("Starting Backuo controller")
-//	defer klog.Info("Shutting down Restore controller")
-//
-//	if ok := cache.WaitForCacheSync(stopCh, c.RestoreSynced); !ok {
-//		klog.Error("Wait For Cache Sync Failed")
-//		return
-//	}
-//
-//	klog.Info("Starting workers")
-//	for i := 0; i < workers; i++ {
-//		go wait.Until(c.worker, time.Second, stopCh)
-//	}
-//
-//	<-stopCh
-//}
-//
-//
-//func (c *RestoreController) worker() {
-//	for c.processNextWorkItem() {
-//	}
-//}
-//
-//func (c *RestoreController) processNextWorkItem() bool {
-//	klog.Info("processNextWorkItem")
-//	key, quit := c.workqueue.Get()
-//	if quit {
-//		return false
-//	}
-//	defer c.workqueue.Done(key)
-//
-//	forget, err := c.syncHandler(key.(string))
-//	if err == nil {
-//		if forget {
-//			c.workqueue.Forget(key)
-//		}
-//		return true
-//	}
-//
-//	utilruntime.HandleError(fmt.Errorf("Error syncing job: %v", err))
-//	c.workqueue.AddRateLimited(key)
-//
-//	return true
-//}
-//
-//func (c *RestoreController) syncRestore(key string) (bool, error) {
-//	klog.Info("Sync Restore: %v", key)
-//	return true, nil
-//}
-//
-//func (c *RestoreController) updateRestore(old, new interface{}) {
-//	c.enqueueRestore(new)
-//}
+import (
+	"fmt"
+	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/kubernetes/pkg/controller"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog"
+
+	mydumperv1alpha1 "github.com/backup-operator/pkg/apis/mydumper/v1alpha1"
+	"github.com/backup-operator/pkg/client/clientset/versioned"
+	clientset "github.com/backup-operator/pkg/client/clientset/versioned"
+	informers "github.com/backup-operator/pkg/client/informers/externalversions"
+	mydumperInformer "github.com/backup-operator/pkg/client/informers/externalversions/mydumper/v1alpha1"
+	mydumperlister "github.com/backup-operator/pkg/client/listers/mydumper/v1alpha1"
+	"k8s.io/client-go/kubernetes/scheme"
+	eventv1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type RestoreController struct {
+	kubeclient  kubernetes.Interface
+	mydumperClient  	clientset.Interface
+
+	mydumperLister        mydumperlister.RestoreLister
+	mydumperInformer     mydumperInformer.RestoreInformer
+	mydumperSynced       cache.InformerSynced
+
+	workqueue workqueue.RateLimitingInterface
+
+	recorder record.EventRecorder
+	cli versioned.Interface
+
+	syncHandler   func(jobKey string) (bool, error)
+
+	expectations controller.ControllerExpectationsInterface
+}
+
+func NewRestoreController(
+	kubeclientset kubernetes.Interface,
+	cli versioned.Interface,
+	genericCli client.Client,
+	informerFactory informers.SharedInformerFactory,
+	kubeInformerFactory kubeinformers.SharedInformerFactory) *MydumperController {
+
+	utilruntime.Must(Restorescheme.AddToScheme(scheme.Scheme))
+	klog.Info("Creating event broadcaster")
+	eventBroadcaster := record.NewBroadcasterWithCorrelatorOptions(record.CorrelatorOptions{QPS: 1})
+	eventBroadcaster.StartLogging(klog.V(2).Infof)
+	eventBroadcaster.StartRecordingToSink(&eventv1.EventSinkImpl{
+		Interface: eventv1.New(kubeclientset.CoreV1().RESTClient()).Events("")})
+	recorder := eventBroadcaster.NewRecorder(mydumperv1alpha1.Scheme, corev1.EventSource{Component: "Restore-controller"})
+
+	nbInformer := informerFactory.Mydumper().V1alpha1().Restores()
+
+	controller := &RestoreController{
+		kubeclient:     				kubeclientset,
+		mydumperInformer:   			nbInformer,
+		mydumperLister:        			nbInformer.Lister(),
+		mydumperSynced:        			nbInformer.Informer().HasSynced,
+		workqueue:         				workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "Restores"),
+		recorder:          				recorder,
+	}
+
+	controller.syncHandler = controller.syncHandler
+
+	klog.Info("Setting up event handlers")
+
+	nbInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: controller.createRestore,
+		UpdateFunc: controller.updateRestore,
+		DeleteFunc: controller.deleteRestore,
+	})
+
+	return controller
+}
+
+func (c *RestoreController) createRestore(obj interface{}) {
+	restore, ok := obj.(*mydumperv1alpha1.Restore)
+
+	if !ok {
+		return
+	}
+
+	return
+}
+
+func (c *RestoreController) deleteRestore(obj interface{}) {
+
+	Restore := obj.(*mydumperv1alpha1.Restore)
+
+}
+
+
+func (c *RestoreController) enqueueRestore(obj interface{}) {
+	var key string
+	var err error
+	if key, err = cache.MetaNamespaceKeyFunc(obj); err != nil {
+		utilruntime.HandleError(err)
+		return
+	}
+	c.workqueue.Add(key)
+}
+
+func (c *RestoreController) handleObject(obj interface{}) {
+	var object metav1.Object
+	var ok bool
+	if object, ok = obj.(metav1.Object); !ok {
+		tombstone, ok := obj.(cache.DeletedFinalStateUnknown)
+		if !ok {
+			utilruntime.HandleError(fmt.Errorf("error decoding object, invalid type"))
+			return
+		}
+		object, ok = tombstone.Obj.(metav1.Object)
+		if !ok {
+			utilruntime.HandleError(fmt.Errorf("error decoding object tombstone, invalid type"))
+			return
+		}
+		klog.Info("Recovered deleted object '%s' from tombstone", object.GetName())
+	}
+	klog.Info("Processing object: %s", object.GetName())
+	if ownerRef := metav1.GetControllerOf(object); ownerRef != nil {
+		// If this object is not owned by a Foo, we should not do anything more
+		// with it.
+		if ownerRef.Kind != "Restore" {
+			return
+		}
+
+		Restore, err := c.mydumperLister.Restores(object.GetNamespace()).Get(ownerRef.Name)
+		if err != nil {
+			klog.Info("ignoring orphaned object '%s' of Restore '%s'", object.GetSelfLink(), ownerRef.Name)
+			return
+		}
+
+		c.enqueueRestore(Restore)
+		return
+	}
+}
+
+func (c *RestoreController) Run(workers int, stopCh <-chan struct{}) {
+	defer utilruntime.HandleCrash()
+	defer c.workqueue.ShutDown()
+
+	klog.Info("Starting Backuo controller")
+	defer klog.Info("Shutting down Restore controller")
+
+	if ok := cache.WaitForCacheSync(stopCh, c.RestoreSynced); !ok {
+		klog.Error("Wait For Cache Sync Failed")
+		return
+	}
+
+	klog.Info("Starting workers")
+	for i := 0; i < workers; i++ {
+		go wait.Until(c.worker, time.Second, stopCh)
+	}
+
+	<-stopCh
+}
+
+
+func (c *RestoreController) worker() {
+	for c.processNextWorkItem() {
+	}
+}
+
+func (c *RestoreController) processNextWorkItem() bool {
+	klog.Info("processNextWorkItem")
+	key, quit := c.workqueue.Get()
+	if quit {
+		return false
+	}
+	defer c.workqueue.Done(key)
+
+	forget, err := c.syncHandler(key.(string))
+	if err == nil {
+		if forget {
+			c.workqueue.Forget(key)
+		}
+		return true
+	}
+
+	utilruntime.HandleError(fmt.Errorf("Error syncing job: %v", err))
+	c.workqueue.AddRateLimited(key)
+
+	return true
+}
+
+func (c *RestoreController) syncRestore(key string) (bool, error) {
+	klog.Info("Sync Restore: %v", key)
+	return true, nil
+}
+
+func (c *RestoreController) updateRestore(old, new interface{}) {
+	c.enqueueRestore(new)
+}
